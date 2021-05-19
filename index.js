a508fabc481aed5a7cb98633d2e8a853e767784d
JoshuaLingbloom <Lingbloomlmp@gmail.com>
2021-05-18 18:02:38 -0700

    commit tree 9ab10edb955d8160759b0136c3e9c7373ab05671

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(streetName){
+    if (streetName >= 42){
+       return streetName - 42;
+    }
+    else return 42 - streetName;
+ }
+function distanceFromHqInFeet(streetName){
+    return distanceFromHqInBlocks(streetName) * 264;
+}
+function distanceTravelledInFeet(start, destination){
+    if (start >= destination){
+        return (start - destination) * 264;
+    }
+    else {return (destination - start) * 264};
+}
+function calculatesFarePrice(start, destination){
+    
+    if (distanceTravelledInFeet(start, destination) <= 400){
+        return 0
+
+    }
+    else if (distanceTravelledInFeet(start, destination) <= 2000){
+        return (distanceTravelledInFeet(start, destination) - 400) * 0.02
+    }
+    else if (distanceTravelledInFeet(start, destination) <= 2500){
+        return 25
+    }
+    else return "cannot travel that far"
+ }
